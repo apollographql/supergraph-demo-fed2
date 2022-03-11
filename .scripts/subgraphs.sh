c2467a72c4925f67f4a530a8fd24626fed043c59
Martin Bonnin <martin@mbonnin.net>
2022-03-11 15:46:47 +0100

    update the location of the different schemas

--- .scripts/subgraphs.sh
+++ .scripts/subgraphs.sh
@@ -6,3 +6,9 @@ url_inventory="http://inventory:4000/graphql"
 url_products="http://products:4000/graphql"
 url_users="http://users:4000/graphql"
 url_pandas="http://pandas:4000/graphql"
+
+schema_inventory="subgraphs/inventory/app/src/main/resources/inventory.graphql"
+schema_products="subgraphs/products/products.graphql"
+schema_users="subgraphs/users/users.graphql"
+schema_pandas="subgraphs/pandas/pandas.graphql"
+
