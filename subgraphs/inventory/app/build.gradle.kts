c99dc2601bb8b12ae374a7d20c39b83607b53f77
Martin Bonnin <martin@mbonnin.net>
2022-03-29 17:43:55 +0200

    use federation-jvm alpha

--- subgraphs/inventory/app/build.gradle.kts
+++ subgraphs/inventory/app/build.gradle.kts
@@ -8,13 +8,15 @@ plugins {
 
 repositories {
   mavenCentral()
+  mavenLocal()
+  maven("repository")
 }
 
 dependencies {
   implementation("com.graphql-java:graphql-java:17.3")
   implementation("com.graphql-java:graphql-java-spring-boot-starter-webmvc:2.0")
   implementation("org.springframework.boot:spring-boot-starter-web")
-  implementation("com.apollographql.federation:federation-graphql-java-support:0.9.0")
+  implementation("com.apollographql.federation:federation-graphql-java-support:2.0.0-alpha.2-SNAPSHOT")
   implementation("org.jetbrains.kotlin:kotlin-reflect")
   implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
   testImplementation("org.springframework.boot:spring-boot-starter-test")
