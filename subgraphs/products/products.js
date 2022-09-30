05c9b5a2debfd357a2f1574b6e690ba17e7f5443
Phil Prasek <prasek@gmail.com>
2022-09-30 12:41:22 -0700

    log subgraph schema for product subgraph
    Signed-off-by: Phil Prasek <prasek@gmail.com>

--- subgraphs/products/products.js
+++ subgraphs/products/products.js
@@ -16,6 +16,7 @@ if (process.env.APOLLO_OTEL_EXPORTER_TYPE) {
 const { ApolloServer, gql } = require('apollo-server');
 const { buildSubgraphSchema } = require('@apollo/subgraph');
 const { readFileSync } = require('fs');
+const { printSchema } = require('graphql');
 
 const port = process.env.APOLLO_PORT || 4000;
 
@@ -80,7 +81,11 @@ const resolvers = {
         }
     }
 }
-const server = new ApolloServer({ schema: buildSubgraphSchema({ typeDefs, resolvers }) });
+const schema = buildSubgraphSchema({ typeDefs, resolvers });
+console.log(`🚀  ApolloServer({ schema: schema }) 🚀`);
+console.log(printSchema(schema));
+
+const server = new ApolloServer({ schema: schema });
 server.listen( {port: port} ).then(({ url }) => {
   console.log(`🚀 Products subgraph ready at ${url}`);
 }).catch(err => {console.error(err)});
