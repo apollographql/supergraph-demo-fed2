98e1964f89ac1f6bd6ef163fcf3ce31d8359fad2
Minghe Huang <minghe.huang@booking.com>
2023-01-15 13:04:16 +0800

    feat(hotels): add hotels subgraph

--- .scripts/subgraphs/docker-compose-networking.sh
+++ .scripts/subgraphs/docker-compose-networking.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
-subgraphs=("inventory" "products" "users" "pandas" "reviews")
+subgraphs=("inventory" "products" "users" "pandas" "reviews" "hotels")
 
 url_inventory="http://inventory:4000/graphql"
 url_products="http://products:4000/graphql"
 url_users="http://users:4000/graphql"
 url_pandas="http://pandas:4000/graphql"
 url_reviews="http://reviews:4000/graphql"
+url_hotels="http://hotels:4000/graphql"
 
 schema_inventory="subgraphs/inventory/app/src/main/resources/graphql/inventory.graphqls"
 schema_products="subgraphs/products/products.graphql"
 schema_users="subgraphs/users/users.graphql"
 schema_pandas="subgraphs/pandas/pandas.graphql"
 schema_reviews="subgraphs/reviews/reviews.graphql"
+schema_hotels="subgraphs/hotels/hotels.graphql"
--- .scripts/subgraphs/localhost-networking.sh
+++ .scripts/subgraphs/localhost-networking.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
-subgraphs=("products" "inventory" "users" "pandas" "reviews")
+subgraphs=("products" "inventory" "users" "pandas" "reviews" "hotels")
 
 url_products="http://localhost:4001/graphql"
 url_inventory="http://localhost:4002/graphql"
 url_users="http://localhost:4003/graphql"
 url_pandas="http://localhost:4004/graphql"
 url_reviews="http://localhost:4005/graphql"
+url_hotels="http://hotels:4006/graphql"
 
 schema_products="subgraphs/products/products.graphql"
 schema_inventory="subgraphs/inventory/app/src/main/resources/graphql/inventory.graphqls"
 schema_users="subgraphs/users/users.graphql"
 schema_pandas="subgraphs/pandas/pandas.graphql"
 schema_reviews="subgraphs/reviews/reviews.graphql"
+schema_hotels="subgraphs/hotels/hotels.graphql"
