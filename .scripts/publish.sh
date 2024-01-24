b5894b13ae1ea26f7485b0927291e6cf6a320bc3
Phil Prasek <prasek@gmail.com>
2024-01-24 15:22:33 -0800

    fix rover error handling
    Signed-off-by: Phil Prasek <prasek@gmail.com>

--- .scripts/publish.sh
+++ .scripts/publish.sh
@@ -9,12 +9,20 @@ echo "======================================="
 source "$(dirname $0)/subgraphs.sh"
 source "$(dirname $0)/graph-api-env.sh"
 
+
+# note: use --allow-invalid-routing-url to allow localhost without confirmation prompt
+
 for subgraph in ${subgraphs[@]}; do
   echo "---------------------------------------"
   echo "subgraph: ${subgraph}"
   echo "---------------------------------------"
   url="url_$subgraph"
   schema="schema_$subgraph"
-  (set -x; ${ROVER_BIN:-'rover'} subgraph publish ${APOLLO_GRAPH_REF} --routing-url "${!url}" --schema "${!schema}" --name ${subgraph} --convert)
+  (set -x; ${ROVER_BIN:-'rover'} subgraph publish ${APOLLO_GRAPH_REF} \
+    --routing-url "${!url}" \
+    --schema "${!schema}" \
+    --name ${subgraph} \
+    --allow-invalid-routing-url \
+    --convert)
   echo ""
 done
